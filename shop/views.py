fae4e3568d63bf196762aecb41099f0bf17e7e7d
Patryk <patryk.kontewicz@poczta.fm>
2019-12-13 21:50:15 +0100

    adding products into cart feature

--- shop/views.py
+++ shop/views.py
@@ -3,22 +3,26 @@ from .models import Category, Product
 
 
 def product_list(request, category_slug=None):
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render(request,
-                  'shop/product/list.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': products})
+  category = None
+  categories = Category.objects.all()
+  products = Product.objects.filter(available=True)
+  if category_slug:
+    category = get_object_or_404(Category, slug=category_slug)
+    products = products.filter(category=category)
+  return render(request,
+                'shop/product/list.html',
+                {'category': category,
+                 'categories': categories,
+                 'products': products})
+
+
+cart_product_form = CartAddProductForm()
 
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id,
-                                slug=slug, available=True)
-    return render(request,
-                  'shop/product/detail.html',
-                  {'product': product})
+  product = get_object_or_404(Product, id=id,
+                              slug=slug, available=True)
+  return render(request,
+                'shop/product/detail.html',
+                {'product': product,
+                 'cart_product_form': cart_product_form})
