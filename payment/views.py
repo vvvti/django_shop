775b4e253bad842f1c9f5b516d7fcd6d9bce7df9
Patryk <patryk.kontewicz@poczta.fm>
2019-12-19 23:27:41 +0100

    created views and urls for payment app

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import braintree
+from django.shortcuts import render, get_object_or_404, redirect
+from orders.models import Order
 
-# Create your views here.
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+
+    if request.method == 'POST':
+        # token nonce
+        nonce = request.POST.get('payment_method_nonce', None)
+        # stworzenie i wysyłka transakcji
+        result = braintree.Transaction.sale({
+            'amount': '{:.2f}'.format(order.get_total_cost()),
+            'payment_method_nonce': nonce,
+            'options': {'submit_for_settelment': True}
+        })
+
+        if result.is_success:
+            # oznaczenie jako opłacone
+            order.paid = True
+            order.braintree_id = result.transaction.id
+            order.save()
+            return redirect('payment:done')
+        else:
+            return redirect('payment:canceled')
+    else:
+        client_token = braintree.ClientToken.generate()
+        return render(request,
+                      'payment/process.html',
+                      {'order': order, 'client_token': client_token})
+
+
+def payment_done(request):
+    return render(request, 'payment/done.html')
+
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
