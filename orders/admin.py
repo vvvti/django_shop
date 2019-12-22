1b30f4e5f6a3a4a08ef6a15e02e3a93cbfaf4cec
Patryk <patryk.kontewicz@poczta.fm>
2019-12-22 13:26:33 +0100

    created custom order view in admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -36,10 +38,15 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_detail(obj):
+    return mark_safe('<a href="{}">Wyświetl</a>'.format(
+        reverse('orders:admin_order_detail', args=[obj.id])))
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
